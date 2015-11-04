86e798514e760d9c89f1b175fa3853f0e1c2e566
Thomas Drevon <thomas@bengler.no>
2015-11-04 17:31:16 +0100

    Extract to BreadCrumbs to its own, clever container

--- components/containers/App.jsx
+++ components/containers/App.jsx
@@ -1,17 +1,12 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
 import * as ImdiPropTypes from '../proptypes/ImdiPropTypes'
+import BreadCrumbs from './BreadCrumbs'
 
 class App extends Component {
   static propTypes = {
     component: PropTypes.func.isRequired,
-    router: ImdiPropTypes.router.isRequired,
-    breadCrumbs: PropTypes.arrayOf(
-      PropTypes.shape({
-        url: PropTypes.string,
-        title: PropTypes.string
-      })
-    )
+    router: ImdiPropTypes.router.isRequired
   }
 
   static childContextTypes = {
@@ -20,7 +15,6 @@ class App extends Component {
   }
 
   static defaultProps = {
-    breadCrumbs: []
   }
 
   getChildContext() {
@@ -33,31 +27,6 @@ class App extends Component {
     }
   }
 
-  renderBreadCrumbs() {
-    const {breadCrumbs} = this.props
-    if (!breadCrumbs) {
-      return null
-    }
-    return (
-      <nav className="breadcrumbs">
-        <ul className="t-no-list-styles breadcrumbs__list">
-          <li key="/" className="breadcrumbs__list-item">
-            <a href="/" className="breadcrumbs__link">Tall og statistikk</a>
-            <span className="breadcrumbs__divider">/</span>
-          </li>
-          {breadCrumbs.map(crumb => (
-            <li key={crumb.url} className="breadcrumbs__list-item">
-              <a href={crumb.url} className="breadcrumbs__link">
-                {crumb.title}
-              </a>
-              <span className="breadcrumbs__divider">/</span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    )
-  }
-
 
   render() {
     return (
@@ -66,7 +35,7 @@ class App extends Component {
           <div className="wrapper">
             <div className="row">
               <div className="col--main-wide">
-                {this.renderBreadCrumbs()}
+                <BreadCrumbs />
               </div>
             </div>
           </div>
@@ -81,8 +50,7 @@ class App extends Component {
 // Note: use https://github.com/faassen/reselect for better performance.
 function select(state) {
   return {
-    component: state.page,
-    breadCrumbs: state.breadCrumbs
+    component: state.page
   }
 }
 
